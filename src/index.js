2ed989d160d8b586945c7089c87de4bd6097a752
nam <johndoe@example.com>
2023-12-25 17:30:04 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const axios = require('axios');
+
+const endpointUrl = 'https://cors-anywhere-nd3f.onrender.com'; // Replace this with the URL you want to send requests to
+const requestInterval = 5000; // Interval in milliseconds (e.g., 5000ms = 5 seconds)
+
+function sendRequest() {
+  axios.get(endpointUrl)
+    .then(response => {
+      console.log('Request sent:', response.status, response.statusText);
+    })
+    .catch(error => {
+      console.error('Error sending request:', error.message);
+    })
+    .finally(() => {
+      setTimeout(sendRequest, requestInterval);
+    });
+}
+
+// Start sending requests
+sendRequest();
