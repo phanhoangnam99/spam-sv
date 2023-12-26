155a36320e82f5125b36f6cfbbd47bf5cca18267
nam <johndoe@example.com>
2023-12-26 10:54:31 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
-const axios = require('axios');
+const axios = require("axios");
+const express = require("express");
+const app = express();
+var port = process.env.PORT || 8080;
+var host = process.env.HOST || "0.0.0.0";
 
-const endpointUrl = 'https://cors-anywhere-nd3f.onrender.com'; // Replace this with the URL you want to send requests to
+const endpointUrl = "https://cors-anywhere-nd3f.onrender.com"; // Replace this with the URL you want to send requests to
 const requestInterval = 60000; // Interval in milliseconds (e.g., 5000ms = 5 seconds)
 
 function sendRequest() {
-  axios.get(endpointUrl)
-    .then(response => {
-      console.log('Request sent:', response.status, response.statusText);
+  axios
+    .get(endpointUrl)
+    .then((response) => {
+      console.log("Request sent:", response.status, response.statusText);
     })
-    .catch(error => {
-      console.error('Error sending request:', error.message);
+    .catch((error) => {
+      console.error("Error sending request:", error.message);
     })
     .finally(() => {
       setTimeout(sendRequest, requestInterval);
@@ -18,3 +23,11 @@ function sendRequest() {
 
 // Start sending requests
 sendRequest();
+
+
+app.get('/',(req,res) => {
+  res.send('hello')
+})
+app.listen(port,()=>{
+  console.log(`Express listening on port ${port} host ${host}`)
+})
