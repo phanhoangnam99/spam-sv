2d6fe6d5bc34da4c34fb8a4369d4a5102e44ab86
nam <johndoe@example.com>
2024-05-02 16:23:55 +0700

    fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const express = require("express");
 const app = express();
 var port = process.env.PORT || 8080;
-var host = process.env.HOST || "127.0.0.1";
+var host = process.env.HOST || "0.0.0.0";
 
 const endpointUrl = "https://cors-anywhere-nd3f.onrender.com"; // Replace this with the URL you want to send requests to
 const requestInterval = 60000; // Interval in milliseconds (e.g., 5000ms = 5 seconds)
